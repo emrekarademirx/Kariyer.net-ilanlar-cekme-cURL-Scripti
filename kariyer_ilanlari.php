2facfe77eb6d3bbdb5e5210cb997c15fcbd960ff
Emre Karademir <99919329+emrekarademirx@users.noreply.github.com>
2023-03-05 01:30:08 +0300

    Create kariyer_ilanlari.php

---
+++ kariyer_ilanlari.php
@@ -0,0 +1,33 @@
+<?php
+$curl = curl_init();
+
+curl_setopt_array($curl, array(
+  CURLOPT_URL => "https://www.kariyer.net/is-ilanlari",
+  CURLOPT_RETURNTRANSFER => true,
+  CURLOPT_ENCODING => "",
+  CURLOPT_MAXREDIRS => 10,
+  CURLOPT_TIMEOUT => 30,
+  CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+  CURLOPT_CUSTOMREQUEST => "GET",
+  CURLOPT_HTTPHEADER => array(
+    "Accept: */*",
+    "Accept-Encoding: gzip, deflate",
+    "Cache-Control: no-cache",
+    "Connection: keep-alive",
+    "Host: www.kariyer.net",
+    "User-Agent: User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/86.0.4240.111 Safari/537.36",
+    "cache-control: no-cache"
+  ),
+));
+
+$response = curl_exec($curl);
+$err = curl_error($curl);
+
+curl_close($curl);
+
+if ($err) {
+  echo "cURL Error #:" . $err;
+} else {
+  echo $response;
+}
+?>
